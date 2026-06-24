002a149b7341d78d2948850eb38b92cc00c49292
Surachai <kbtlqcdepartment@gmail.com>
2026-06-24 11:21:37 +0700

    Update inspector.js

--- js/inspector.js
+++ js/inspector.js
@@ -1,1 +1,20 @@
-// Inspector module
+fetch("https://script.google.com/macros/s/AKfycby-rz0BE7Q04J7CixQ4XRYJMcbo4Wo1WqtvdZ3I7amsrUGgA_kAhLEdiOHGBuJV74tj/exec")
+
+.then(response => response.json())
+
+.then(data => {
+
+    let box = document.getElementById("inspector");
+
+    data.forEach(name => {
+
+        let option = document.createElement("option");
+
+        option.text = name;
+        option.value = name;
+
+        box.add(option);
+
+    });
+
+});
