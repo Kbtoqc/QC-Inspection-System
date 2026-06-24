a6825fbcd7dae22e6c568565ed7d8a11f980d968
Surachai <kbtlqcdepartment@gmail.com>
2026-06-24 12:52:18 +0700

    Update material.js

--- js/material.js
+++ js/material.js
@@ -1,1 +1,22 @@
-// Material module
+const MATERIAL_API = "URL_WEB_APP_MATERIAL_TYPE";
+
+fetch(MATERIAL_API)
+
+.then(response => response.json())
+
+.then(data => {
+
+    const materialBox = document.getElementById("material");
+
+    data.forEach(material => {
+
+        const option = document.createElement("option");
+
+        option.textContent = material;
+        option.value = material;
+
+        materialBox.appendChild(option);
+
+    });
+
+});
