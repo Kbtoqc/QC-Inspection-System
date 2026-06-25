c4d27bb6b8eb4dbffaa7d887bb90786d62b0ff8f
Surachai <kbtlqcdepartment@gmail.com>
2026-06-25 20:18:20 +0700

    Update saveInspection.js

--- js/saveInspection.js
+++ js/saveInspection.js
@@ -1,1 +1,11 @@
 console.log("Save JS Loaded");
+
+
+document.addEventListener("DOMContentLoaded", function(){
+
+  const saveButton = document.getElementById("saveInspection");
+
+  console.log("Save Button:", saveButton);
+
+
+});
