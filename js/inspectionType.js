c0dfe040289696bd621bc669ff09257e606b9976
Surachai <kbtlqcdepartment@gmail.com>
2026-06-25 16:05:41 +0700

    Update inspectionType.js

--- js/inspectionType.js
+++ js/inspectionType.js
@@ -1,1 +1,42 @@
-// Product module
+document.addEventListener("DOMContentLoaded", () => {
+
+  const INSPECTION_TYPE_API =
+  "https://script.google.com/macros/s/AKfycbyKuiMu4hfPttljc3HnF7hdKE2HP8UG7z68KMCvkk8GSBG6bLJeuL-aeirobW3VDJ9o/exec";
+
+
+  const typeBox = document.getElementById("inspectionType");
+
+
+  fetch(INSPECTION_TYPE_API)
+
+    .then(response => response.json())
+
+    .then(data => {
+
+      typeBox.innerHTML =
+        '<option value="">Select Inspection Type</option>';
+
+
+      data.forEach(name => {
+
+        const option = document.createElement("option");
+
+        option.value = name;
+        option.textContent = name;
+
+        typeBox.appendChild(option);
+
+      });
+
+    })
+
+    .catch(error => {
+
+      console.error("Inspection Type Load Error:", error);
+
+      typeBox.innerHTML =
+        '<option value="">Load Failed</option>';
+
+    });
+
+});
