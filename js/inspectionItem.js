96672e42578f4af9fa571262437c4cb727d1474c
Surachai <kbtlqcdepartment@gmail.com>
2026-06-25 16:59:36 +0700

    Update inspectionItem.js

--- js/inspectionItem.js
+++ js/inspectionItem.js
@@ -1,43 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-
   const INSPECTION_ITEM_API =
   "https://script.google.com/macros/s/AKfycbw9QKqQNuHD5nQdP7MTwDOc5vu4nyMFCgeeHsXnG7dZYxrykaCf2yJv4-ZU36CvYvYU/exec";
 
 
   const materialBox = document.getElementById("material");
-
-  const inspectionBody =
-    document.getElementById("inspectionBody");
+  const inspectionBody = document.getElementById("inspectionBody");
 
 
   let inspectionItems = [];
 
 
-  // Load checklist master
-
   fetch(INSPECTION_ITEM_API)
-
     .then(response => response.json())
-
     .then(data => {
 
       inspectionItems = data;
 
-    })
+      console.log("Inspection Item Loaded:", inspectionItems);
 
+    })
     .catch(error => {
 
-      console.error(
-        "Inspection Item Load Error:",
-        error
-      );
+      console.error("Inspection Item Load Error:", error);
 
     });
 
 
-
-  // เมื่อเลือก Material Type
 
   materialBox.addEventListener("change", () => {
 
@@ -48,113 +37,50 @@ document.addEventListener("DOMContentLoaded", () => {
     inspectionBody.innerHTML = "";
 
 
-    if (!materialType) {
-
-      return;
-
-    }
+    const checklist = inspectionItems.filter(item =>
+      item.materialType === materialType
+    );
 
 
-    const checklist =
-      inspectionItems.filter(item =>
-        item.materialType === materialType
-      );
+    checklist.forEach(item => {
 
 
-
-    checklist.forEach((item, index) => {
-
-
-      const row =
-      document.createElement("tr");
+      const row = document.createElement("tr");
 
 
-
-      // Result Column
-
-      let resultInput = "";
-
+      let resultField = "";
 
 
       if (item.resultType === "OKNGNA") {
 
-
-        resultInput = `
-
-          <select class="result">
-
-            <option value="">
-              Select
-            </option>
-
-            <option value="OK">
-              OK
-            </option>
-
-            <option value="NG">
-              NG
-            </option>
-
-            <option value="NA">
-              NA
-            </option>
-
+        resultField = `
+          <select>
+            <option value="">Select</option>
+            <option value="OK">OK</option>
+            <option value="NG">NG</option>
+            <option value="NA">NA</option>
           </select>
-
         `;
 
+      } else {
 
-      } else if (item.resultType === "Value") {
-
-
-        resultInput = `
-
-          <input 
-            type="text"
-            class="result"
-            placeholder="Enter Value">
-
+        resultField = `
+          <input type="text">
         `;
-
 
       }
 
 
-
       row.innerHTML = `
 
+        <td>${item.itemNo}</td>
+        <td>${item.checkPoint}</td>
+        <td>${item.spec}</td>
+        <td>${item.checkType}</td>
+        <td>${resultField}</td>
         <td>
-          ${item.itemNo}
+          <input type="text">
         </td>
-
-
-        <td>
-          ${item.checkPoint}
-        </td>
-
-
-        <td>
-          ${item.spec}
-        </td>
-
-
-        <td>
-          ${item.checkType}
-        </td>
-
-
-        <td>
-          ${resultInput}
-        </td>
-
-
-        <td>
-          <input 
-            type="text"
-            class="remark"
-            placeholder="Remark">
-        </td>
-
 
       `;
 
