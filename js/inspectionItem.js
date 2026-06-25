7a7934327f2bdabb56cd9c62be28f4ae5d54bab6
Surachai <kbtlqcdepartment@gmail.com>
2026-06-25 19:33:39 +0700

    Update inspectionItem.js

--- js/inspectionItem.js
+++ js/inspectionItem.js
@@ -1,28 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
 
   const INSPECTION_ITEM_API =
-  "https://script.google.com/macros/s/AKfycbw9QKqQNuHD5nQdP7MTwDOc5vu4nyMFCgeeHsXnG7dZYxrykaCf2yJv4-ZU36CvYvYU/exec";
-
+    "https://script.google.com/macros/s/AKfycbw9QKqQNuHD5nQdP7MTwDOc5vu4nyMFCgeeHsXnG7dZYxrykaCf2yJv4-ZU36CvYvYU/exec";
 
   const materialBox = document.getElementById("material");
   const inspectionBody = document.getElementById("inspectionBody");
 
-
   let inspectionItems = [];
 
-
   fetch(INSPECTION_ITEM_API)
-
     .then(response => response.json())
-
     .then(data => {
 
       inspectionItems = data;
 
-      console.log("Inspection Item Loaded", inspectionItems);
+      console.log("Inspection Item Loaded");
 
     })
-
     .catch(error => {
 
       console.error("Inspection Item Load Error:", error);
@@ -30,32 +24,24 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 
-
-  materialBox.addEventListener("change", function() {
-
+  materialBox.addEventListener("change", function () {
 
     const selectedMaterial = this.value;
 
-
     inspectionBody.innerHTML = "";
-
 
     const items = inspectionItems.filter(item =>
       item.materialType === selectedMaterial
     );
 
-
     items.forEach(item => {
-
 
       let resultField = "";
 
-
       if (item.resultType === "OKNGNA") {
 
-
         resultField = `
-          <select>
+          <select class="result">
             <option value="">Select</option>
             <option value="OK">OK</option>
             <option value="NG">NG</option>
@@ -63,48 +49,31 @@ document.addEventListener("DOMContentLoaded", () => {
           </select>
         `;
 
-
-      } else if (item.resultType === "Value") {
-
+      } else {
 
         resultField = `
-          <input type="text">
+          <input type="text" class="result">
         `;
-
 
       }
 
-
-
       const row = document.createElement("tr");
 
-
       row.innerHTML = `
-
         <td>${item.itemNo}</td>
-
         <td>${item.checkPoint}</td>
-
         <td>${item.spec}</td>
-
         <td>${item.checkType}</td>
-
         <td>${resultField}</td>
-
         <td>
-          <input type="text">
+          <input type="text" class="remark">
         </td>
-
       `;
-
 
       inspectionBody.appendChild(row);
 
-
     });
 
-
   });
-
 
 });
