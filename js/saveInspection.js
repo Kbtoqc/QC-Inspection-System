78a485774b150198f852b97c89e6b7829bb5b1c3
Surachai <kbtlqcdepartment@gmail.com>
2026-06-25 20:38:53 +0700

    Update saveInspection.js

--- js/saveInspection.js
+++ js/saveInspection.js
@@ -12,7 +12,63 @@ if(saveButton){
 
   saveButton.addEventListener("click", function(){
 
-    console.log("Save Button Clicked");
+
+    const header = {
+
+
+      inspectionDate:
+      document.getElementById("inspectionDate").value,
+
+
+      soNo:
+      document.getElementById("soNo").value,
+
+
+      poNo:
+      document.getElementById("poNo").value,
+
+
+      inspector:
+      document.getElementById("inspector").value,
+
+
+      customer:
+      document.getElementById("customer").value,
+
+
+      supplier:
+      document.getElementById("supplier").value,
+
+
+      inspectionType:
+      document.getElementById("inspectionType").value,
+
+
+      materialType:
+      document.getElementById("material").value,
+
+
+      model:
+      document.getElementById("model").value,
+
+
+      serialNo:
+      document.getElementById("serialNo").value,
+
+
+      finalJudgement:
+      document.getElementById("finalJudgement").value,
+
+
+      finalRemark:
+      document.getElementById("finalRemark").value
+
+
+    };
+
+
+    console.log("Header Data:", header);
+
 
   });
 
