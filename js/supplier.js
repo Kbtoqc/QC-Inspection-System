9ea447a7df027b731562ed7418845909038a531d
Surachai <kbtlqcdepartment@gmail.com>
2026-06-25 15:58:47 +0700

    Update supplier.js

--- js/supplier.js
+++ js/supplier.js
@@ -1,19 +1,41 @@
-fetch("https://script.google.com/macros/s/AKfycbxMucxispr3qhpxDVZ0-WnCdlzO3hs_KRXVORVIsid_olm33zvU02TTzYdT-iBntLCU/exec")
+document.addEventListener("DOMContentLoaded", () => {
 
-.then(response => response.json())
+  const SUPPLIER_API =
+  "https://script.google.com/macros/s/AKfycbxMucxispr3qhpxDVZ0-WnCdlzO3hs_KRXVORVIsid_olm33zvU02TTzYdT-iBntLCU/exec";
 
-.then(data => {
 
-    let box = document.getElementById("supplier");
+  const supplierBox = document.getElementById("supplier");
 
-    data.forEach(item => {
 
-        let option = document.createElement("option");
+  fetch(SUPPLIER_API)
 
-        option.text = item;
-        option.value = item;
+    .then(response => response.json())
 
-        box.add(option);
+    .then(data => {
+
+      supplierBox.innerHTML =
+        '<option value="">Select Supplier</option>';
+
+
+      data.forEach(name => {
+
+        const option = document.createElement("option");
+
+        option.value = name;
+        option.textContent = name;
+
+        supplierBox.appendChild(option);
+
+      });
+
+    })
+
+    .catch(error => {
+
+      console.error("Supplier Load Error:", error);
+
+      supplierBox.innerHTML =
+        '<option value="">Load Failed</option>';
 
     });
 
